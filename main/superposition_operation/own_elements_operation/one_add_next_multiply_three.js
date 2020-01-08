84302b38c17efd3ca24822a4ae4c3aaf66d31c2f
GraceeFlower <1491644103@qq.com>
2020-01-08 17:52:22 +0800

    [WangMin] refactor: replace the 'var' with 'let' in es6 & delete the redundant return sentence

--- main/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
+++ main/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
@@ -1,12 +1,11 @@
 'use strict';
 function one_add_next_multiply_three(collection) {
-  var res = [];
-  for(var item = 0; item < collection.length; item++) {
-    if(item + 1 !== collection.length) {
-      var multi = (collection[item] + collection[item + 1]) * 3;
-      res.push(multi);
+  let res = [];
+  collection.forEach((item, index, arr) => {
+    if (index + 1 !== arr.length) {
+      res.push((item + arr[index + 1]) * 3);
     }
-  }
+  });
   return res;
 }
 
