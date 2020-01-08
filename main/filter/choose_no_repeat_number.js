d5de58aabdbc1f012ffe6bb94434a680d5d207a6
GraceeFlower <1491644103@qq.com>
2020-01-08 17:04:55 +0800

    [WangMin] refactor: use arrow function in es6 to replace the normal sentences

--- main/filter/choose_no_repeat_number.js
+++ main/filter/choose_no_repeat_number.js
@@ -1,9 +1,8 @@
 'use strict';
 
 function choose_no_repeat_number(collection) {
- return collection.filter(function (value, index, array) {
-    return array.indexOf(value) === index;
-  });
+ return collection.filter((value, index, array) =>
+          array.indexOf(value) === index);
 }
 
 module.exports = choose_no_repeat_number;
