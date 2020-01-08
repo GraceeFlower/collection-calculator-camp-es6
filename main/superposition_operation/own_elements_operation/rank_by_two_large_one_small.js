6e44c667dbec1ee65f56912bced2479af06159f3
GraceeFlower <1491644103@qq.com>
2020-01-08 17:53:22 +0800

    [WangMin] refactor: replace the 'var' with 'let' in es6

--- main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -1,10 +1,10 @@
 'use strict';
 function rank_by_two_large_one_small(collection) {
-  var arr = collection.sort((pre, cur) => pre - cur);
-  var res = [];
-  for(var item = 0; item < arr.length;) {
+  let arr = collection.sort((pre, cur) => pre - cur);
+  let res = [];
+  for(let item = 0; item < arr.length;) {
     if (item + 3 < arr.length) {
-      var subArr = arr.slice(item, item + 3);
+      let subArr = arr.slice(item, item + 3);
       subArr.shift();
       subArr.push(arr[item]);
       res = res.concat(subArr);
