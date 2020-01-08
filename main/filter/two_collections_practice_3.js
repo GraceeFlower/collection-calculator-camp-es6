144880e9f780e097999bd667e895f208ab2fd39f
GraceeFlower <1491644103@qq.com>
2020-01-08 17:10:26 +0800

    [WangMin] refactor: delete the redundant return in the arrow function

--- main/filter/two_collections_practice_3.js
+++ main/filter/two_collections_practice_3.js
@@ -1,9 +1,8 @@
 'use strict';
 
 function choose_divisible_integer(collection_a, collection_b) {
-  return collection_a.filter((value) => {
-    return collection_b.some((item) => !(value % item));
-  });
+  return collection_a.filter((value) => 
+    collection_b.some((item) => !(value % item)));
 }
 
 module.exports = choose_divisible_integer;
