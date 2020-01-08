b12920aa446811b44c7d870c0675517c33746338
GraceeFlower <1491644103@qq.com>
2020-01-08 16:36:44 +0800

    [WangMin] refactor: use forEach & arrow function in es6 to replace for loop sentences

--- main/collectionOperator/get_intersection.js
+++ main/collectionOperator/get_intersection.js
@@ -1,17 +1,12 @@
 'use strict';
 
 function get_intersection(collection_a, collection_b) {
-  var res = [];
-  for(var bItem = 0; bItem < collection_b.length; bItem++) {
-    for(var aItem = 0; aItem < collection_a.length;) {
-      if(collection_a[aItem] === collection_b[bItem]) {
-        res.push(collection_b[bItem]);
-        break;
-      } else {
-        aItem++;
-      }
-    }
-  }
+  let res = [];
+  collection_b.forEach(item => {
+    collection_a.forEach(value => {
+      if (value === item) {res.push(item);}
+    });
+  });
   return res;
 }
 
