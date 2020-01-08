0fbf46cc13eeaa84c77a46ea6e6c85c14a97ec27
GraceeFlower <1491644103@qq.com>
2020-01-08 17:43:04 +0800

    [WangMin] refactor: replace the 'var' with 'let' in es6

--- main/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
@@ -1,5 +1,5 @@
 'use strict';
-let calculate_average = function(collection){
+let calculate_average = function (collection){
   let res = collection.filter((value, index) => index % 2 !== 0);
   return res.reduce((pre, cur) => pre + cur) / res.length;
 };
