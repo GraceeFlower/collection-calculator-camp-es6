e1324822c30a5f71c2fc9ec6adda8f74fbbd58e5
GraceeFlower <1491644103@qq.com>
2020-01-08 17:27:57 +0800

    [WangMin] refactor: replace the 'var' with 'let' in es6

--- main/section_1/practice_4/practice.js
+++ main/section_1/practice_4/practice.js
@@ -1,8 +1,8 @@
 function collect_same_elements(collection_a, object_b) {
-  var arr_a = collection_a.map((item) => item.key);
-  var arr_b = object_b.value;
-  var totalArr = arr_b.concat(arr_a);
-  var res = totalArr.filter(function (value, index, array) {
+  let arr_a = collection_a.map((item) => item.key);
+  let arr_b = object_b.value;
+  let totalArr = arr_b.concat(arr_a);
+  let res = totalArr.filter(function (value, index, array) {
     return array.indexOf(value) !== index;
   }, arr_b.length);
   return res;
