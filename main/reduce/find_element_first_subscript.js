2c21c852b6d9e5cd7d513c9cb2c9e6ee8929343b
GraceeFlower <1491644103@qq.com>
2020-01-08 17:20:01 +0800

    [WangMin] refactor: replace the 'var' with 'let' in es6

--- main/reduce/find_element_first_subscript.js
+++ main/reduce/find_element_first_subscript.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function calculate_elements_sum(collection, element) {
-  var item = 0;
+  let item = 0;
   while(item < collection.length) {
     if (collection[item] === element) {
       return item;
