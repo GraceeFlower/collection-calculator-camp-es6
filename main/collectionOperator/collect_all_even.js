1c506d8f0ddbeb0c0c550eb8185bc444f3948aa6
GraceeFlower <1491644103@qq.com>
2020-01-08 16:21:03 +0800

    [WangMin] refactor: change the filter function into es6

--- main/collectionOperator/collect_all_even.js
+++ main/collectionOperator/collect_all_even.js
@@ -1,9 +1,7 @@
 'use strict';
 
 function collect_all_even(collection) {
-  return collection.filter(function (item) {
-    return item % 2 === 0;
-  });
+  return collection.filter(item => item % 2 === 0);
 }
 
 module.exports = collect_all_even;
