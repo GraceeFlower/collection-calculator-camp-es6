4805773c20da39dc76e7feb1e0381ae045be2c37
GraceeFlower <1491644103@qq.com>
2020-01-08 17:49:08 +0800

    [WangMin] refactor: replace the 'var' with 'let' in es6

--- main/superposition_operation/own_elements_operation/even_asc_odd_desc.js
+++ main/superposition_operation/own_elements_operation/even_asc_odd_desc.js
@@ -1,7 +1,7 @@
 'use strict';
-var even_asc_odd_desc = function (collection) {
-  var evenArr = collection.filter((item) => item % 2 === 0);
-  var oddArr = collection.filter((item) => item % 2 !== 0);
+let even_asc_odd_desc = function (collection) {
+  let evenArr = collection.filter((item) => item % 2 === 0);
+  let oddArr = collection.filter((item) => item % 2 !== 0);
   evenArr.sort((pre, cur) => pre - cur);
   oddArr.sort((pre, cur) => cur - pre);
   return evenArr.concat(oddArr);
