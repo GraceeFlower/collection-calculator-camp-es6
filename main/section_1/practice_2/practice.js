9ba824c5b690e066663a11dc377ecb41433e2537
GraceeFlower <1491644103@qq.com>
2020-01-08 17:25:54 +0800

    [WangMin] refactor: use arrow function in es6 to replace the normal function & delete the redudant return sentence

--- main/section_1/practice_2/practice.js
+++ main/section_1/practice_2/practice.js
@@ -1,10 +1,8 @@
 function collect_same_elements(collection_a, collection_b) {
   var totalArr = collection_b.concat(collection_a);
   totalArr = double_to_one(totalArr);
-  var res = totalArr.filter(function (value, index, array) {
-    return array.indexOf(value) !== index;
-  }, collection_b.length);
-  return res;
+  return totalArr.filter((value, index, array) =>
+    array.indexOf(value) !== index, collection_b.length);
 }
 
 function double_to_one(collection) {
