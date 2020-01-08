820c5c386602f9fb3b4b541c4efc02d05d3639ba
GraceeFlower <1491644103@qq.com>
2020-01-08 17:23:20 +0800

    [WangMin] refactor: use arrow function in es6 to replace the normal function

--- main/section_1/practice_1/practice.js
+++ main/section_1/practice_1/practice.js
@@ -1,8 +1,7 @@
 function collect_same_elements(collection_a, collection_b) {
   var totalArr = collection_b.concat(collection_a);
-  return totalArr.filter(function (value, index, array) {
-    return array.indexOf(value) !== index;
-  }, collection_b.length);
+  return totalArr.filter((value, index, array) =>
+    array.indexOf(value) !== index, collection_b.length);
 }
 
 module.exports = collect_same_elements;
