64e8cee8f67a2d56075da767937009c7cb8df4ea
GraceeFlower <1491644103@qq.com>
2020-01-08 17:02:53 +0800

    [WangMin] refactor: replace the 'var' with 'let' in es6

--- main/collectionOperator/get_union.js
+++ main/collectionOperator/get_union.js
@@ -1,8 +1,8 @@
 'use strict';
 
 function get_union(collection_a, collection_b) {
-  var res = collection_a.concat(collection_b);
-  for(var item = collection_a.length; item < res.length;) {
+  let res = collection_a.concat(collection_b);
+  for(let item = collection_a.length; item < res.length;) {
     if (res.indexOf(res[item]) !== item) {
       res.splice(item, 1);
     } else {
