f6edb105864605c23f16a7aa5218241821b24034
GraceeFlower <1491644103@qq.com>
2020-01-08 18:24:08 +0800

    [WangMin] fix: declare the subArr out of the if-else sentence

--- main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -3,8 +3,9 @@ function rank_by_two_large_one_small(collection) {
   let arr = collection.sort((pre, cur) => pre - cur);
   let res = [];
   for(let item = 0; item < arr.length;) {
+    let subArr;
     if (item + 3 < arr.length) {
-      let subArr = arr.slice(item, item + 3);
+      subArr = arr.slice(item, item + 3);
       subArr.shift();
       subArr.push(arr[item]);
       res = res.concat(subArr);
