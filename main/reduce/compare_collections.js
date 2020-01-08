a035f2bdd7a03228355b378f6e229edebd5b3c00
GraceeFlower <1491644103@qq.com>
2020-01-08 17:16:55 +0800

    [WangMin] refactor: use arrow function in es6 to replace the normal function

--- main/reduce/compare_collections.js
+++ main/reduce/compare_collections.js
@@ -1,13 +1,10 @@
 'use strict';
 
 function compare_collections(collection_a, collection_b) {
-  var res = true;
+  let res = true;
   if (collection_a.length === collection_b.length) {
-    collection_a.reduce(function (pre, cur, index, array) {
-      if (array[index] !== collection_b[index]) {
-        res = false;
-      }
-    });
+    collection_a.forEach((value, index) => {
+      if (value !== collection_b[index]) res = false;});
   } else {
     res = false;
   }
