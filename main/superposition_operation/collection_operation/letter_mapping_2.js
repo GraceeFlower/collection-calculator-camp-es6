9821656909259ee6cb134ecc39c36741a88b6bad
GraceeFlower <1491644103@qq.com>
2020-01-08 17:38:25 +0800

    [WangMin] refactor: replace the 'var' with 'let' in es6

--- main/superposition_operation/collection_operation/letter_mapping_2.js
+++ main/superposition_operation/collection_operation/letter_mapping_2.js
@@ -1,15 +1,15 @@
 'use strict';
 
 function average_to_letter(collection) {
-  var res = (collection.reduce((pre, cur) => pre + cur))/collection.length;
+  let res = (collection.reduce((pre, cur) => pre + cur))/collection.length;
   return generateString(Math.ceil(res));
 }
 
 function generateString(index) {
-  var excess = Math.floor((index - 1) / 26);
-  var basic = 96 + index;
-  var former = 96 + excess;
-  var latter = 96 + index - excess * 26;
+  let excess = Math.floor((index - 1) / 26);
+  let basic = 96 + index;
+  let former = 96 + excess;
+  let latter = 96 + index - excess * 26;
   return (excess ? String.fromCharCode(former) + String.fromCharCode(latter) : 
     String.fromCharCode(basic));
 }
