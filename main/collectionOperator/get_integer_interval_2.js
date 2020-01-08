7f5650db82a3b5d6752f633addc2f6c0664aa8a0
GraceeFlower <1491644103@qq.com>
2020-01-08 16:25:48 +0800

    [WangMin] refactor: replace the 'var' with 'let' in es6

--- main/collectionOperator/get_integer_interval_2.js
+++ main/collectionOperator/get_integer_interval_2.js
@@ -1,15 +1,15 @@
 'use strict';
 
 function get_integer_interval_2(number_a, number_b) {
-  var res = [];
+  let res = [];
   if (number_a < number_b) {
-    var item = number_a % 2 ? number_a + 1 : number_a;
+    let item = number_a % 2 ? number_a + 1 : number_a;
     while(item <= number_b) {
       res.push(item);
       item += 2;
     }
   } else if(number_a > number_b) {
-    var item = number_a % 2 ? number_a - 1 : number_a;
+    let item = number_a % 2 ? number_a - 1 : number_a;
     while(item >= number_b) {
       res.push(item);
       item -= 2;
