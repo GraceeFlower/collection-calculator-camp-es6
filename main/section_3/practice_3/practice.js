a12cbbad43e91cab41ee152adeff7295944c86c1
GraceeFlower <1491644103@qq.com>
2020-01-08 17:35:10 +0800

    [WangMin] refactor: replace the 'var' with 'let' in es6

--- main/section_3/practice_3/practice.js
+++ main/section_3/practice_3/practice.js
@@ -1,21 +1,21 @@
 function create_updated_collection(collection_a, object_b) {
   collection_a = count_same_elements(collection_a);
   object_b.value.map((item) => {
-    var index = checkItem(collection_a, item);
-    var count = collection_a[index].count;
+    let index = checkItem(collection_a, item);
+    let count = collection_a[index].count;
     collection_a[index].count -= Math.floor(count / 3);
   });
   return collection_a;
 }
 
 function count_same_elements(collection) {
-  var results = [];
-  collection.forEach(function (value) {
-    var item = checkItem(results, value);
+  let results = [];
+  collection.forEach((value) => {
+    let item = checkItem(results, value);
     if (item !== "") {
       results[item].count++;
     } else {
-      var result = {};
+      let result = {};
       result.key = value;
       result.count = 1;
       results.push(result);
@@ -25,7 +25,7 @@ function count_same_elements(collection) {
 }
 
 function checkItem(arr, element) {
-  for(var item = 0; item < arr.length; item++) {
+  for(let item = 0; item < arr.length; item++) {
     if (arr[item].key === element) {
       return item;
     }
