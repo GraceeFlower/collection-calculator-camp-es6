23db1f953ef40774132809a5e98f2e46c7d4eb3a
GraceeFlower <1491644103@qq.com>
2020-01-10 19:07:56 +0800

    [WangMin] refactor: use includes function to simplify the problem

--- main/section_1/practice_4/practice.js
+++ main/section_1/practice_4/practice.js
@@ -1,9 +1,6 @@
 function collect_same_elements(collection_a, object_b) {
-  let arr_a = collection_a.map((item) => item.key);
-  let arr_b = object_b.value;
-  let totalArr = arr_b.concat(arr_a);
-  return totalArr.filter((value, index, array) =>
-    array.indexOf(value) !== index, arr_b.length);
+  return collection_a.map((item) => item.key).filter(item => 
+    object_b.value.includes(item));
 }
 
 module.exports = collect_same_elements;
