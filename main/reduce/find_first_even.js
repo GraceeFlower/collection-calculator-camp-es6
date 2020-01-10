255f1c7a0dcdbe07dea3b9ff411b8eebb9a4feec
GraceeFlower <1491644103@qq.com>
2020-01-10 18:53:05 +0800

    [WangMin] refactor: use find function to solve the finding first even problem

--- main/reduce/find_first_even.js
+++ main/reduce/find_first_even.js
@@ -1,15 +1,7 @@
 'use strict';
 
 function find_first_even(collection) {
-  let item = 0;
-  while(item < collection.length) {
-    if (collection[item] % 2) {
-      item++;
-    } else {
-      return collection[item];
-    }
-  }
-  return null;
+  return collection.find(item => item % 2 === 0);
 }
 
 module.exports = find_first_even;
