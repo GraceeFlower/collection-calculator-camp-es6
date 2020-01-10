9f8743a75bc7c1ca1ae6c03ff064d7240596103b
GraceeFlower <1491644103@qq.com>
2020-01-10 18:50:03 +0800

    [WangMin] refactor: use findIndex & reverse functions to do

--- main/reduce/find_element_last_subscript.js
+++ main/reduce/find_element_last_subscript.js
@@ -1,17 +1,8 @@
 'use strict';
 
 function calculate_elements_sum(collection, element) {
-  let item = 0;
-  let res;
-  while(item < collection.length) {
-    if (collection[item] === element) {
-      res = item;
-      item++;
-    } else {
-      item++;
-    }
-  }
-  return res;
+  return (collection.length - 1) - collection.reverse().findIndex(
+    item => item === element);
 }
   
 module.exports = calculate_elements_sum;
