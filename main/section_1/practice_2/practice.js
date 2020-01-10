3035bca972d5e0938afe7b4c6a9bd6b94cecb05d
GraceeFlower <1491644103@qq.com>
2020-01-10 19:04:13 +0800

    [WangMin] refactor: use flat & includes functions to simplify the problem

--- main/section_1/practice_2/practice.js
+++ main/section_1/practice_2/practice.js
@@ -1,12 +1,5 @@
 function collect_same_elements(collection_a, collection_b) {
-  let totalArr = collection_b.concat(collection_a);
-  totalArr = double_to_one(totalArr);
-  return totalArr.filter((value, index, array) =>
-    array.indexOf(value) !== index, collection_b.length);
-}
-
-function double_to_one(collection) {
-  return collection.reduce((pre, cur) => pre.concat(cur), []);
+  return collection_a.filter(item => collection_b.flat().includes(item));
 }
 
 module.exports = collect_same_elements;
