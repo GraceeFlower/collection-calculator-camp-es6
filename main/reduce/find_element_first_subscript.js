2a90ff7d50375bf5ed4aa0b0cb7fc633f333bc1f
GraceeFlower <1491644103@qq.com>
2020-01-10 18:42:44 +0800

    [WangMin] refactor: use findIndex function to choose the index of the item

--- main/reduce/find_element_first_subscript.js
+++ main/reduce/find_element_first_subscript.js
@@ -1,14 +1,7 @@
 'use strict';
 
 function calculate_elements_sum(collection, element) {
-  let item = 0;
-  while(item < collection.length) {
-    if (collection[item] === element) {
-      return item;
-    } else {
-      item++;
-    }
-  }
+  return collection.findIndex(item => element === item);
 }
 
 module.exports = calculate_elements_sum;
