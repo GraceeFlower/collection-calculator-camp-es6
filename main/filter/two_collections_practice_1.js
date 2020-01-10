0c3de2fddb9b08097048db1b4ebcd476b14039b6
GraceeFlower <1491644103@qq.com>
2020-01-10 18:31:11 +0800

    [WangMin] refactor: simplify the function to choose the common elements

--- main/filter/two_collections_practice_1.js
+++ main/filter/two_collections_practice_1.js
@@ -1,9 +1,7 @@
 'use strict';
 
 function choose_common_elements(collection_a, collection_b) {
-  let totalArr = collection_b.concat(collection_a);
-  return totalArr.filter((value, index, array) => 
-    array.indexOf(value) !== index, collection_b.length);
+  return collection_a.filter(element => collection_b.includes(element));
 }
 
 module.exports = choose_common_elements;
--- main/filter/two_collections_practice_2.js
+++ main/filter/two_collections_practice_2.js
@@ -2,7 +2,7 @@
 
 function choose_no_common_elements(collection_a, collection_b) {
   return collection_a.filter((value) => 
-    !collection_b.includes(value));
+ !collection_b.includes(value));
 }
 
 module.exports = choose_no_common_elements;
